7d482639b47a072dc74afb3bee4bb81f8f5ca000
Suxrob <suxidev@gmail.com>
2023-12-27 17:44:50 +0500

    feat: added category views

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import news_list
+from .views import news_list, category_news
 
 
 urlpatterns = [
-    path('', news_list, name='news_list' )
+    path('', news_list, name='news_list' ),
+    path('new/<slug:slug>', category_news, name='category_news' ),
+
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from .models import City, Category, News
 
+def category(request):
+    categories = Category.objects.all()
+
+    context = {
+        'categories': categories,
+    }
+
+    return context
+
+def city(request):
+    cities = City.objects.all()
+
+    context = {
+        'cities': cities,
+    }
+
+    return context
+
+
 def news_list(request):
     news = News.objects.all()
 
@@ -8,3 +27,15 @@ def news_list(request):
         'news': news,
     }
     return render(request, 'news_list.html', context)
+
+
+def category_news(request, slug):
+    category = Category.objects.get(slug = slug)
+    news = News.objects.filter(category = category)
+
+    context = {
+        'news': news,
+        'category': category,
+    }
+
+    return render(request,'category.html',  context)
